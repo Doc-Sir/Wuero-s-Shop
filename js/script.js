3579a725529cf08b53e91f3b0e0e77befb7fc6a7
Doc-Sir <145786854+Doc-Sir@users.noreply.github.com>
2023-11-30 10:46:45 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,124 +1,53 @@
-const password = "nofish1234";
+const password = "nofishs1234";
 let authenticated = false;
 
 function authenticate() {
-    const passwordInput = document.getElementById("password");
-    const quantityInput = document.getElementById("quantity");
-    const passwordValue = passwordInput.value.trim();
-    
-    if (passwordValue === password) {
-        authenticated = true;
-        alert("Authentication successful!");
-        passwordInput.value = "";
-        quantityInput.value = "";
-        passwordInput.disabled = true;
-        quantityInput.disabled = false;
-    } else {
-        alert("Incorrect password");
-        passwordInput.value = "";
-        quantityInput.value = "";
-    }
+  const passwordInput = document.getElementById("password");
+  const quantityInput = document.getElementById("quantity");
+  const passwordValue = passwordInput.value.trim();
+
+  if (passwordValue === password) {
+    authenticated = true;
+    alert("Authentication successful!");
+    passwordInput.value = "";
+    quantityInput.value = "";
+    passwordInput.disabled = true;
+    quantityInput.disabled = false;
+
+    const items = document.querySelectorAll(".item");
+    const passwordInputContainer = document.querySelector(".password-input");
+    const passwordSubmitButton = document.querySelector(".password-submit");
+
+    items.forEach(item => {
+      item.style.display = "none";
+    });
+
+    passwordInputContainer.style.display = "block";
+    passwordSubmitButton.style.display = "block";
+  } else {
+    alert("Incorrect password");
+    passwordInput.value = "";
+    quantityInput.value = "";
+  }
 }
 
 function changeQuantity() {
-    if (authenticated) {
-        const quantityInput = document.getElementById("quantity");
-        const quantityValue = parseInt(quantityInput.value.trim());
-        const items = document.querySelectorAll(".item");
-        
-        items.forEach(item => {
-            const quantityElement = item.querySelector(".quantity");
-            const id = quantityElement.id;
-            const quantity = quantityValue || 0;
-            
-            quantityElement.textContent = `Quantity: ${quantity}`;
-        });
-        
-        alert("Quantity updated successfully!");
-        quantityInput.value = "";
-    } else {
-        alert("Please authenticate first!");
-    }
-}const password = "password123";
-let authenticated = false;
-
-function authenticate() {
-    const passwordInput = document.getElementById("password");
+  if (authenticated) {
     const quantityInput = document.getElementById("quantity");
-    const passwordValue = passwordInput.value.trim();
-    
-    if (passwordValue === password) {
-        authenticated = true;
-        alert("Authentication successful!");
-        passwordInput.value = "";
-        quantityInput.value = "";
-        passwordInput.disabled = true;
-        quantityInput.disabled = false;
-    } else {
-        alert("Incorrect password");
-        passwordInput.value = "";
-        quantityInput.value = "";
-    }
-}
+    const quantityValue = parseInt(quantityInput.value.trim());
+    const items = document.querySelectorAll(".item");
 
-function changeQuantity() {
-    if (authenticated) {
-        const quantityInput = document.getElementById("quantity");
-        const quantityValue = parseInt(quantityInput.value.trim());
-        const items = document.querySelectorAll(".item");
-        
-        items.forEach(item => {
-            const quantityElement = item.querySelector(".quantity");
-            const id = quantityElement.id;
-            const quantity = quantityValue || 0;
-            
-            quantityElement.textContent = `Quantity: ${quantity}`;
-        });
-        
-        alert("Quantity updated successfully!");
-        quantityInput.value = "";
-    } else {
-        alert("Please authenticate first!");
-    }
-}const password = "password123";
-let authenticated = false;
+    items.forEach(item => {
+      const quantityElement = item.querySelector(".quantity");
+      const id = quantityElement.id;
+      const quantity = quantityValue || 0;
 
-function authenticate() {
-    const passwordInput = document.getElementById("password");
-    const quantityInput = document.getElementById("quantity");
-    const passwordValue = passwordInput.value.trim();
-    
-    if (passwordValue === password) {
-        authenticated = true;
-        alert("Authentication successful!");
-        passwordInput.value = "";
-        quantityInput.value = "";
-        passwordInput.disabled = true;
-        quantityInput.disabled = false;
-    } else {
-        alert("Incorrect password");
-        passwordInput.value = "";
-        quantityInput.value = "";
-    }
-}
+      quantityElement.textContent = `Quantity: ${quantity}`;
+    });
 
-function changeQuantity() {
-    if (authenticated) {
-        const quantityInput = document.getElementById("quantity");
-        const quantityValue = parseInt(quantityInput.value.trim());
-        const items = document.querySelectorAll(".item");
-        
-        items.forEach(item => {
-            const quantityElement = item.querySelector(".quantity");
-            const id = quantityElement.id;
-            const quantity = quantityValue || 0;
-            
-            quantityElement.textContent = `Quantity: ${quantity}`;
-        });
-        
-        alert("Quantity updated successfully!");
-        quantityInput.value = "";
-    } else {
-        alert("Please authenticate first!");
-    }
+    alert("Quantity updated successfully!");
+    quantityInput.value = "";
+  } else {
+    alert("Please authenticate first!");
+  }
 }
